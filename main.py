810df157763633263a0037af5b298bb73d4733ba
Jack Yan <jackyanart@gmail.com>
2019-02-21 16:43:07 -0500

    Add MainLogic class and Classifier skeleton. Further logic needs to be implemented

--- main.py
+++ main.py
@@ -2,51 +2,111 @@ import sys, time
 
 sys.path.insert(0, 'hardware')
 from controller import Controller
+import camera_config
 
 sys.path.insert(0, 'software')
-import classiflier
+import classiflier as clr
 
 LEFT = 0 # Counter Clockwise
 RIGHT = 1 # Clockwise
+FORWARD = 2
 
-def sensor_hit_object(controller):
-	distance = abs(controller.get_distance())
-	print(distance)
-	return distance < 35
+STATE_SEARCH = 0
+STATE_DITCH = 1
 
-def get_distance(controller):
-	return abs(controller.get_distance())
+class MainLogic():
+	def __init__(self):
+		self.controller = Controller()
+		self.classifier = clr.Classifier()
+		self.state = STATE_SEARCH
+		self.aura = 35 # objects within 35 units will be approached by rover
+		self.resolution_width = camera_config.resolution_width,
+		self.resolution_height = camera_config.resolution_height
 
-def main():
-	c = Controller()
+	def get_distance(self):
+		return abs(self.controller.get_distance())
 
-	while 1:
-		# Camera to identify object direction (left/right) and object classification
+	# ************ loop methods ************
+	def loop(self):
+		if self.state == STATE_SEARCH:
+			self.search_object()
+		elif self.state == STATE_DITCH:
+			self.search_ditch()
+
+	def search_object(self):
+		# Step 1: Camera to identify object direction (left/right) and object classification
 		# Classifier API returns object type and object coordinates
+		objects = self.classifier.process(self.controller.capture_opencv())
 		direction = LEFT
+		object_type = clr.OBJECT_NONE
+		for obj in objects:
+			if obj[1] != clr.OBJECT_NONE:
+				if obj[0][0] < self.resolution_width:
+					direction = LEFT
+				elif obj[0][0] > self.resolution_width:
+					direction = RIGHT
+				else:
+					direction = FORWARD
+				object_type = obj[1]
 
+
+
+		# Step 2: Sweep with rover to identify objects (needs refinement)
 		# Sensor to identify distance to object by turning the rover towards left/right
-		l = 13
-		running_distances = []
-		while 1: # can even use classifier here to check object coord
-			distance = get_distance(c)
+		l = 5
+		running_distances = [999] * l
+		while direction != FORWARD:
+			distance = self.get_distance()
 			print(distance)
 			running_distances.append(distance)
+
 			if len(running_distances) > l:
 				running_distances.pop(0)
-			if len(running_distances) == l and sum(running_distances) / l < 35:
+
+			if self.object_ahead(running_distances):
 				break
 
-			c.turn(direction = direction)
-			time.sleep(0.01)
-			c.stop()
+			self.turn(direction = direction, delay = 0.01)
 
-		# Classifier to see whether object is in center of image
 
-		# Go straight because object in front of rover
-		c.move(direction = 0);
-		time.sleep(0.5)
-		c.stop()
+		# Step 3: Go straight because object in front of rover
+		# Move forward at small intervals until object is within grasp of rover
+		while distance > 5:
+			self.move(direction = 0, speed = self.get_speed_from_distance(distance), delay = 0.1)
+			distance = self.get_distance()
+
+	def search_ditch(self):
+		# TODO
+		return 0
+
+
+	# ************ supporting methods ************
+	def turn(self, direction = 0, speed = 255, delay = 0.1):
+		self.controller.turn(direction = direction, speed = speed)
+		time.sleep(delay)
+		self.controller.stop()
+
+	def move(self, direction = 0, speed = 255, delay = 0.1):
+		self.controller.move(direction = direction, speed = speed)
+		time.sleep(delay)
+		self.controller.stop()
+
+	def object_ahead(self, running_distances):
+		# Use Classifier to identify object coordinate, 
+		# if object in center and sensor returns some resonable value then object is ahead
+		return sum(running_distances) / len(running_distances) < self.aura
+	def get_speed_from_distance(self, distance):
+		# Calculate rover moving speed based on distance from object
+		sMax = 255
+		sMin = 1
+		factor = 2 # higher it is slower the movement
+		speed = (sMax-sMin) * distance / (factor * self.aura)
+		return speed if speed < sMax else sMax
+
+def main():
+	m = MainLogic()
+	while 1:
+		m.loop()		
 		return
 
 main()
--- software/classiflier.py
+++ software/classiflier.py
@@ -1,1 +1,23 @@
-import nanpy
+import numpy
+
+OBJECT_NONE = -1
+OBJECT_CANS = 0
+OBJECT_PAPER = 1
+
+class Classifier():
+	def __init__(self):
+		self.dummy = 0
+
+	def process(self, img):
+		# takes in image in certain format (please advise)
+		# return a list of coordinates of objects. (-1,-1) if no object found
+		# also returns classification of the objects
+		
+		return [([-1,-1], OBJECT_NONE)]
+
+	def process_ditch(self, img):
+		# takes in image of certain format
+		# returns the type of ditch that is perpendicular to rover
+		# return OBJECT_NONE if no ditch is perpendicular
+
+		return OBJECT_NONE
