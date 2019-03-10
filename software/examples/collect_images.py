ba8c1d03c395831b58df47b698a5c51f80b8ee34
Jack Yan <jackyanart@gmail.com>
2019-03-10 15:23:13 -0400

    New v

--- software/examples/collect_images.py
+++ software/examples/collect_images.py
@@ -1,13 +1,18 @@
 import sys
 sys.path.insert(0,"/home/pi/Capstone-GC/hardware")
 from controller import Controller
+from PIL import Image
 
 c = Controller()
 
-name = "images/positive-"
-num = 0
+name = "images/ditch"
+num = 60
 
 while True:
-	input("take pic")
-	c.capture(name + str(num) + ".jpg")
+	input("take pic {}".format(num))
+	_name = name + str(num) + ".jpg"
+	c.capture(_name)
+	img = Image.open(_name)
+	img = img.rotate(180)
+	img.save(_name)
 	num += 1
